7763c3b09e23f98b7119d8be50ad9b27bc1dd5f8
Paul Thomas <paul@asup.co.uk>
2022-02-07 11:53:15 +0000

    Update table options button

--- src/components/ait/AsupInternalTable.jsx
+++ src/components/ait/AsupInternalTable.jsx
@@ -39,6 +39,7 @@ export const AsupInteralTable = ({
   const [bodyData, setBodyData] = useState(initialData.bodyData ?? {});
   const [options, setOptions] = useState(initialData.options ?? {});
   const [showOptions, setShowOptions] = useState(false);
+  const [showOptionsButton, setShowOptionsButton] = useState(false);
   const [currentLocation, setCurrentLocation] = useState({});
 
   const focusedElement = useActiveElement();
@@ -59,8 +60,8 @@ export const AsupInteralTable = ({
   }, [focusedElement])
 
   // Show or hide style buttons
-  // const aitShowProperties = () => { if (showOptions) { setOptionsView(showOptions); } };
-  // const aitHideProperties = () => { setOptionsView("hidden"); };
+  const aitShowProperties = () => { setShowOptionsButton(true); };
+  const aitHideProperties = () => { setShowOptionsButton(false); };
 
   // Collate and return data
   useEffect(() => {
@@ -78,12 +79,12 @@ export const AsupInteralTable = ({
     <>
       <div
         className="ait-holder"
-        // onMouseOver={aitShowProperties}
-        // onMouseLeave={aitHideProperties}
+        onMouseOver={aitShowProperties}
+        onMouseLeave={aitHideProperties}
         style={addStyle}
       >
-        <div style={{alignContent:"right"}}>
-          <div className='ait-table-options' style={{  }}>
+        <div>
+          <div className={`ait-table-options  ${showOptionsButton ? "visible" : "hidden"}`} onClick={(e) => {console.log('click show optins'); setShowOptions(true);}}>
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-fill" viewBox="0 0 16 16" onClick={(e) => { setShowOptions(true); }}>
               <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
             </svg>
@@ -120,6 +121,7 @@ export const AsupInteralTable = ({
       <AsupInternalWindow
         Title={"Table options"}
         Visible={showOptions}
+        onClose={() => {setShowOptions(false);} }
       >
         <div className='aiw-sub-title'><small>Table</small></div>
         <AioOptionGroup initialData={options} returnData={setOptions} />
