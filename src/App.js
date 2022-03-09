29ba98ec8b3be2cc7545a7b6f09b3da7cdff957c
Paul Thomas <paul@asup.co.uk>
2022-03-09 12:29:12 +0000

    Drag and drop example added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { TablePage } from './pages/TablePage';
 import "./pages/pages.css";
 import { WindowPage } from './pages/WindowPage';
 import { ExpanderPage } from 'pages/ExpanderPage';
+import { Table2 } from 'pages/TablePage2';
 
 const activeClass = ({ isActive }) => (isActive ? 'active' : 'inactive');
 
@@ -21,6 +22,7 @@ function App() {
           <ul>
             <li><NavLink to="/"         className={activeClass}>Current </NavLink></li>
             <li><NavLink to="/table"    className={activeClass}>Table   </NavLink></li>
+            <li><NavLink to="/table2"   className={activeClass}>TableDND</NavLink></li>
             <li><NavLink to="/expander" className={activeClass}>Expander</NavLink></li>
             <li><NavLink to="/editor"   className={activeClass}>Editor  </NavLink></li>
             <li><NavLink to="/window"   className={activeClass}>Window  </NavLink></li>
@@ -28,8 +30,9 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/"         element={<ExpanderPage />   } />
+          <Route path="/"         element={<TablePage />      } />
           <Route path="/table"    element={<TablePage />   } />
+          <Route path="/table2"    element={<Table2 />   } />
           <Route path="/editor"   element={<EditorPage />  } />
           <Route path="/expander" element={<ExpanderPage />} />
           <Route path="/window"   element={<WindowPage />  } />
