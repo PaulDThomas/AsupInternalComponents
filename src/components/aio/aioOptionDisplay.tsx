5f290d133b152e5cde1b39467a91d63ba2dedba2
Paul Thomas <paul@asup.co.uk>
2022-03-17 07:22:05 +0000

    Remove option display update

--- src/components/aio/aioOptionDisplay.tsx
+++ src/components/aio/aioOptionDisplay.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import { AioOption, AioOptionGroup } from "./aioInterface";
 import { AioPrintOption } from "./aioPrintOption";
 import "./aio.css";
@@ -11,39 +11,18 @@ interface AioOptionDisplayProps {
 
 export const AioOptionDisplay = (props: AioOptionDisplayProps): JSX.Element => {
 
-  // Data holder
-  const [options, setOptions] = useState((props.initialData ?? []).map(a => { return { ...a } }));
-  const originalOptions = useRef((props.initialData ?? []).map(a => { return { ...a } }));
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [buttonText] = useState(props.buttonText ?? "Update");
-
-  // Update on a load...
-  useEffect(() => {
-    var newDataCheck = false;
-    for (let i in props.initialData) {
-      if (JSON.stringify(props.initialData[i]) !== JSON.stringify(originalOptions.current[i])) {
-        newDataCheck = true;
-        //console.log("Found new data");
-      }
-    }
-    if (newDataCheck) {
-      // console.log("Initial data update");
-      setOptions((props.initialData ?? []).map(a => { return { ...a } }));
-      originalOptions.current = (props.initialData ?? []).map(a => { return { ...a } });
-    }
-  }, [props.initialData]);
-
   // Update current options from child object
   const updateOption = (ret: AioOption, i: number) => {
+    if (typeof (props.returnData) !== "function") return;
     // console.log(`Updating option ${i} to... ${ret}`);
-    const newOptions = [...options];
+    const newOptions = [...props.initialData];
     newOptions[i].value = ret;
-    setOptions(newOptions);
+    props.returnData!(newOptions);
   };
 
   return (
     <>
-      {options.map((option, i) => {
+      {props.initialData.map((option, i) => {
         return (
           <div className='aio-body-row' key={i}>
             <AioPrintOption
@@ -57,9 +36,6 @@ export const AioOptionDisplay = (props: AioOptionDisplayProps): JSX.Element => {
           </div>
         );
       })}
-      <div style={{ width: "100%", textAlign: "center" }}>
-        <button className={"aio-update-button"} onClick={(e: React.MouseEvent<Element, MouseEvent>) => { if (typeof (props.returnData) === "function") props.returnData(options); }}>{buttonText}</button>
-      </div>
     </>
   );
 }
