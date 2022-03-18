6da48e378caae132207ad2639ef0023b87d2da48
Paul Thomas <paul@asup.co.uk>
2022-03-18 08:57:54 +0000

    Fix objEqual

--- src/components/ait/processes.ts
+++ src/components/ait/processes.ts
@@ -36,21 +36,23 @@ export const processOptions = (incomingOptions: AioOptionGroup, previousOptions:
 export const objEqual = (a: any, b: any, path?: string): [boolean, string] => {
   // console.log(`Checking ${path}`);
   if (a === b) return [true, ""];
-  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) return [a === b, (a === b) ? "" : `${path}:notMismatch:${a}:${b}`];
-  if (a === null || a === undefined || b === null || b === undefined) return [false, `${path}:nullMismatch:${a}:${b}`];
-  if (a.prototype !== b.prototype) return [false, `${path}:prototypeMismatch:${a}!==${b}`];
+  if (typeof a !== "object" && typeof b !== "object" && a !== b) return [false, `${path}:notEqual:${a}<>${b}`];
+  if (!a || !b || (typeof a !== 'object' && typeof b !== 'object')) return [a === b, (a === b) ? "" : `${path}:notMismatch:${a}<>${b}`];
+  if (a === null || a === undefined || b === null || b === undefined) return [false, `${path}:nullMismatch:${a}<>${b}`];
+  if (a.prototype !== b.prototype) return [false, `${path}:prototypeMismatch:${a}<>${b}`];
   let keys = Object.keys(a);
-  if (keys.length !== Object.keys(b).length) return [false, `${path}:lengthMismatch:${keys.length}:${Object.keys(b).length}`];
+  if (keys.length !== Object.keys(b).length) return [false, `${path}:lengthMismatch:${keys.length}<>${Object.keys(b).length}`];
   let checkObject = keys
-    .map(k => objEqual(a[k], b[k], k))
+    .map(k => objEqual(a[k], b[k], `${path}.${k}`));
+  let checkObjectR = checkObject
     .reduce(([overall, str], [b, s]) => {
       let tf = (overall === true && b === true);
-      let badPath = (s !== "") ? `${str}${str !== "" ? "\n" : ""}${s}` : "";
+      let badPath = (s !== "") ? `${str}${str !== "" ? "\n" : ""}${s}` : str;
       return [tf, badPath];
     }
       , [true, ""]
     );
-  return checkObject;
+  return checkObjectR;
 }
 
 export const getReplacementValues = (rvs: AioReplacementValue[]): [number[][], string[][]] => {
