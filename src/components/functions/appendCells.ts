4c54435eaf0165296d60bed484fed927b4d8c48a
Paul Thomas <paul@asup.co.uk>
2022-03-30 16:08:49 +0100

    Utility function

---
+++ src/components/functions/appendCells.ts
@@ -0,0 +1,16 @@
+import { AitRowData } from "components/ait/aitInterface";
+
+/**
+ * Add another block of cells onto the right of a row
+ * @param base Initial block of rows
+ * @param block of rows to add to the right
+ * @returns Consolidated rows
+ */
+export const appendCells = (base: AitRowData[], append: AitRowData[]): AitRowData[] => {
+  return base.map((r, ri) => {
+    return {
+      aitid: r.aitid,
+      cells: [...r.cells, ...append[ri]?.cells],
+    } as AitRowData;
+  });
+};
