a38581d99eb72519ed98c163c07f44982d9dcd1a
Paul Thomas <paul@asup.co.uk>
2022-04-04 09:59:57 +0100

    Fix for missing target

--- src/components/functions/createRepeats.ts
+++ src/components/functions/createRepeats.ts
@@ -31,6 +31,8 @@ export const createRepeats = (
     let repVal: string[] = repeats.values !== undefined ? repeats.values[repi] : [];
     /** First row number that needs to be repeated for this level */
     let firstLevel: number = repi > 0 ? firstUnequal(repNo, repeats.numbers[repi - 1]) : 0;
+    if (targetArray[firstLevel] === undefined)
+      return false;
     /** Rows that need to be repeated for this level */
     let slice = repi === 0 ? rows : rows.slice(targetArray[firstLevel].row);
     if (slice.length === 0)
