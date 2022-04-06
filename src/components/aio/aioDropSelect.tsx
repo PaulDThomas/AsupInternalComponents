b8b5c003a58a66cd779c6af50050e108561dc834
Paul Thomas <paul@asup.co.uk>
2022-04-06 15:22:48 +0100

    Handle clicks outside node for AioDropSelect

--- src/components/aio/aioDropSelect.tsx
+++ src/components/aio/aioDropSelect.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 
 interface AioDropSelectProps {
   value?: string,
@@ -10,6 +10,23 @@ export const AioDropSelect = ({ value, setValue, availableValues }: AioDropSelec
 
   const [showDrop, setShowDrop] = useState<boolean>(false);
 
+  // Set up outsideClick handler
+  const menuRef = useRef<HTMLDivElement | null>(null);
+
+  const handleClick = useCallback((e: MouseEvent) => {
+    if (menuRef.current && (
+      (e.target instanceof Element && !menuRef.current?.contains(e.target))
+      || !(e.target instanceof Element)
+    )) {
+      setShowDrop(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (showDrop) document.addEventListener('mousedown', handleClick);
+    else document.removeEventListener('mousedown', handleClick);
+  }, [handleClick, showDrop]);
+
   return (
     <>
       <div>
@@ -18,14 +35,14 @@ export const AioDropSelect = ({ value, setValue, availableValues }: AioDropSelec
             marginRight: value !== undefined ? "0.25rem" : "0"
           }}
         >{value}</span>
-        {typeof setValue === "function" &&
+        {typeof setValue === "function" && (availableValues?.length ?? 0) > 1 &&
           <div className="aiox closed" style={{ position: "relative" }}>
             <div className='aiox-button'
               onClick={() => { setShowDrop(!showDrop); }
               }
             />
             {showDrop &&
-              <div className="aio-drop-items-holder"
+              <div className="aio-drop-items-holder" ref={menuRef}
                 style={{
                   position: "absolute",
                   zIndex: 2000,
@@ -44,9 +61,9 @@ export const AioDropSelect = ({ value, setValue, availableValues }: AioDropSelec
                       <div
                         key={i}
                         className={`aio-drop-item`}
-                        onClick={e => { setValue(a); setShowDrop(false) }}
+                        onClick={e => { setValue(a); setShowDrop(false); }}
                       >
-                        {a}
+                        {a.replace(/ /g, "\u00A0")}
                       </div>
                     )
                   }
