8db1070f6264a5e5006e3f4702f6e759fb5a97f6
Paul Thomas <paul@asup.co.uk>
2022-04-14 15:28:45 +0100

    Add to/from html functions

---
+++ src/components/functions/tofromHtml.ts
@@ -0,0 +1,21 @@
+export const toHtml = (text: string): string =>
+  text
+    .replace(/&/g, "&amp;")
+    .replace(/>/g, "&gt;")
+    .replace(/</g, "&lt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;")
+    .replace(/\n/g, "<br/>")
+    .replace(/ /g, "&nbsp;")
+  ;
+
+export const fromHtml = (text: string): string =>
+  text
+    .replace(/&nbsp;/g, " ")
+    .replace(/<br\/>/g, "\n")
+    .replace(/&gt;/g, ">")
+    .replace(/&lt;/g, "<")
+    .replace(/&quot;/g, '"')
+    .replace(/&apos;/g, "'")
+    .replace(/&amp;/g, "&")
+  ;
