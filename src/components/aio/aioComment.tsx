2185d95fd61b0b241212065e4d04a6f3c7669775
Paul Thomas <paul@asup.co.uk>
2022-04-16 12:11:23 +0100

    Use AIE for AioComment

--- src/components/aio/aioComment.tsx
+++ src/components/aio/aioComment.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import { AsupInternalEditor } from "../aie";
 import { AioLabel } from "./aioLabel";
 
 interface AioCommentProps {
   label?: string,
-  value?: string,
+  value: string,
   setValue?: (value: string) => void,
 }
 
@@ -12,21 +13,16 @@ export const AioComment = (props: AioCommentProps): JSX.Element => {
     <>
       <AioLabel label={props.label} />
       <div className={"aio-input-holder"}>
-        {(typeof (props.setValue) !== "function")
+        {typeof props.setValue === "function"
           ?
-          <span>{props.value}</span>
-          :
-          <textarea
-            className={"aio-comment"}
-            value={props.value ?? ""}
-            rows={4}
-            onChange={typeof (props.setValue) === "function"
-              ?
-              (e: React.ChangeEvent<HTMLTextAreaElement>) => { if (props.setValue) props.setValue(e.currentTarget.value); }
-              :
-              undefined
-            }
+          <AsupInternalEditor
+            showStyleButtons={false}
+            value={props.value}
+            setValue={(ret) => props.setValue!(ret)}
+            editable={true}
           />
+          :
+          <span>{props.value}</span>
         }
       </div>
     </>
