c3726fb8f087d782e48c67c5d714a13493dbd018
PaulDThomas <paul@asup.co.uk>
2022-04-28 16:01:32 +0100

    Add functions

---
+++ src/components/functions/newRow.ts
@@ -0,0 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+import { AitCellType, AitRowData, AitRowType } from "../ait/aitInterface";
+import { newCell } from "./newCell";
+
+/**
+ * Create new table row
+ * @param l Length of row
+ * @param type Type of row to be created, header or body
+ * @returns New row data
+ */
+export const newRow = (l: number, type?: AitRowType): AitRowData => {
+  let newRow: AitRowData = { aitid: uuidv4(), cells: [], };
+  // Add new cells in a loop to avoid duplicate aitid
+  for (let i = 0; i < l; i++)
+    newRow.cells.push(newCell(type === AitRowType.header ? AitCellType.header : AitCellType.body));
+  return newRow;
+};
