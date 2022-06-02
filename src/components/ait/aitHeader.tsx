bb225ec966be8d08fdcbebe9323e1db6c620a915
Paul Thomas <paul@asup.co.uk>
2022-06-02 10:39:34 +0100

    Fix row removal bug

--- src/components/ait/aitHeader.tsx
+++ src/components/ait/aitHeader.tsx
@@ -72,7 +72,7 @@ export const AitHeader = ({
         // Adjust the rowSpan of the cell above
         for (let i = 1; i < c.rowSpan!; i++) {
           if (newRows[ri + i].cells[ci].rowSpan === 0) {
-            newRows[ri + 1].cells[ci].rowSpan = 1;
+            newRows[ri + i].cells[ci].rowSpan = 1;
           }
         }
       }
