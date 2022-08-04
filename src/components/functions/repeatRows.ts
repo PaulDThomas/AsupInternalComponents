176c5dd20024b649c668234e5afee08588ecdece
Paul Thomas <paul@asup.co.uk>
2022-08-04 15:43:35 +0200

    Allow space after when no repeats

--- src/components/functions/repeatRows.ts
+++ src/components/functions/repeatRows.ts
@@ -39,50 +39,57 @@ export const repeatRows = (
   let extReplacements = updateExternals(replacements, externalLists);
 
   // Check other spaceAfter if there are replacements
-  if ((extReplacements?.length ?? 0) > 0) for (let si = 0; si < extReplacements!.length; si++) {
-    // Data fix
-    if (si > 0 && extReplacements![si].includeTrailing) {
-      extReplacements![si].includeTrailing = false;
-    }
-    // Run current replacement
-    newRows = replaceRows(newRows, extReplacements![si]);
-    // Add spaceAfter for the group
-    if (spaceAfter) {
-      newRows[newRows.length - 1].spaceAfter = true;
-    }
-    // Process spaceAfter
-    for (let ri = 0; ri < newRows.length; ri++) {
-      // Start cell counter, Look down each column
-      for (let ci = 0; ci < newRows[0].cells.length; ci++) {
-        // Cycle through each column cell
-        for (let ri = 0; ri < newRows.length; ri++) {
-          let targetCell = newRows[ri].cells[ci];
-          // Add space below appropriate rows if required
-          if (
-            (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) > 0 &&
-            targetCell.spaceAfterRepeat &&
-            ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1 < newRows.length &&
-            !newRows[ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1].spaceAfter
-          ) {
-            newRows[ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1].spaceAfter = true;
-            let lookback = 1;
-            // Check previous cells
-            while (lookback <= ci) {
-              let lookup = 0;
-              let found = false;
-              while (lookup <= ri && !found) {
-                let checkCell = newRows[ri - lookup].cells[ci - lookback];
-                if ((checkCell.rowSpan !== 0 && (checkCell.repeatRowSpan ?? checkCell.rowSpan ?? 1) > 1)) {
-                  found = true;
-                  checkCell.spaceAfterSpan = (checkCell.spaceAfterSpan ?? 0) + 1;
+  if ((extReplacements?.length ?? 0) > 0) {
+    for (let si = 0; si < extReplacements!.length; si++) {
+      // Data fix
+      if (si > 0 && extReplacements![si].includeTrailing) {
+        extReplacements![si].includeTrailing = false;
+      }
+      // Run current replacement
+      newRows = replaceRows(newRows, extReplacements![si]);
+      // Add spaceAfter for the group
+      if (spaceAfter) {
+        newRows[newRows.length - 1].spaceAfter = true;
+      }
+      // Process spaceAfter
+      for (let ri = 0; ri < newRows.length; ri++) {
+        // Start cell counter, Look down each column
+        for (let ci = 0; ci < newRows[0].cells.length; ci++) {
+          // Cycle through each column cell
+          for (let ri = 0; ri < newRows.length; ri++) {
+            let targetCell = newRows[ri].cells[ci];
+            // Add space below appropriate rows if required
+            if (
+              (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) > 0 &&
+              targetCell.spaceAfterRepeat &&
+              ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1 < newRows.length &&
+              !newRows[ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1].spaceAfter
+            ) {
+              newRows[ri + (targetCell.repeatRowSpan ?? targetCell.rowSpan ?? 1) - 1].spaceAfter = true;
+              let lookback = 1;
+              // Check previous cells
+              while (lookback <= ci) {
+                let lookup = 0;
+                let found = false;
+                while (lookup <= ri && !found) {
+                  let checkCell = newRows[ri - lookup].cells[ci - lookback];
+                  if ((checkCell.rowSpan !== 0 && (checkCell.repeatRowSpan ?? checkCell.rowSpan ?? 1) > 1)) {
+                    found = true;
+                    checkCell.spaceAfterSpan = (checkCell.spaceAfterSpan ?? 0) + 1;
+                  }
+                  lookup++;
                 }
-                lookup++;
+                lookback++;
               }
-              lookback++;
             }
           }
         }
       }
+    }
+  }
+  else {
+    if (spaceAfter) {
+      newRows[newRows.length - 1].spaceAfter = true;
     }
   }
 
