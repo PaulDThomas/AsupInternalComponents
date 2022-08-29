13509671126e64b28781850abf19dde3d97cecf2
Paul Thomas <paul@asup.co.uk>
2022-08-29 21:21:22 +0100

    Prettier aif

--- src/components/aif/aifInterface.ts
+++ src/components/aif/aifInterface.ts
@@ -1,17 +1,17 @@
 export interface AifBlockLine {
-  aifid?: string, // Unique ID
-  left?: string | false, // Left aligned text in the row
-  centre?: string | false, // Centre aligned text in the row
-  right?: string | false, // Right aligned text in the row
-  addBelow?: boolean, // If the user can add a line below
-  canEdit?: boolean, // If the user can edit the line
-  canRemove?: boolean, // If the user can remove the line
-  canMove?: boolean, // If the user can move the line in an array of lines
+  aifid?: string; // Unique ID
+  left?: string | false; // Left aligned text in the row
+  centre?: string | false; // Centre aligned text in the row
+  right?: string | false; // Right aligned text in the row
+  addBelow?: boolean; // If the user can add a line below
+  canEdit?: boolean; // If the user can edit the line
+  canRemove?: boolean; // If the user can remove the line
+  canMove?: boolean; // If the user can move the line in an array of lines
 }
 
 export const enum AifLineType {
-  leftOnly = "Left only",
-  centreOnly = "Centre only",
-  leftAndRight = "Left and Right", 
-  leftCentreAndRight = "Left, Centre and Right"
+  leftOnly = 'Left only',
+  centreOnly = 'Centre only',
+  leftAndRight = 'Left and Right',
+  leftCentreAndRight = 'Left, Centre and Right',
 }
--- src/components/aif/replaceBlockText.ts
+++ src/components/aif/replaceBlockText.ts
@@ -1,14 +1,22 @@
 import { newReplacedText } from '../aie/functions/newReplacedText';
 import { AioExternalSingle } from '../aio';
 
-export const replaceBlockText = (text?: string | false, rep?: AioExternalSingle): { newText: string | false | undefined; updated: boolean; } => {
+export const replaceBlockText = (
+  text?: string | false,
+  rep?: AioExternalSingle,
+): { newText: string | false | undefined; updated: boolean } => {
   // Stop processing
-  if (text === undefined || text === false ||
-    rep === undefined || rep.oldText === undefined || rep.oldText === "" || rep.newText === undefined)
+  if (
+    text === undefined ||
+    text === false ||
+    rep === undefined ||
+    rep.oldText === undefined ||
+    rep.oldText === '' ||
+    rep.newText === undefined
+  )
     return { newText: text, updated: false };
   // Not found
-  if (!text.includes(rep.oldText))
-    return { newText: text, updated: false };
+  if (!text.includes(rep.oldText)) return { newText: text, updated: false };
   // Make replacement
   return { newText: newReplacedText(text, rep.oldText, rep.newText), updated: true };
 };
