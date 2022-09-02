41614920191654872f5faebd6e36cc812a077c51
Paul Thomas <paul@asup.co.uk>
2022-09-02 17:51:59 +0100

    Add formatting into EditorV2 return

--- src/components/aie/functions/getHTMLfromV2Text.tsx
+++ src/components/aie/functions/getHTMLfromV2Text.tsx
@@ -1,12 +1,22 @@
-export function getHTMLfromV2Text(text: string, styleName: string): string {
+export function getHTMLfromV2Text(
+  text: string,
+  styleName: string,
+  style: React.CSSProperties,
+): string {
   if (styleName === '') return text;
   const isr = {
     length: text.length,
     offset: 0,
     style: styleName,
   };
+  const cssString = Object.entries(style)
+    .map(([k, v]) => `${k.replace(/[A-Z]/g, '-$&').toLowerCase()}:${v}`)
+    .join(';');
   return `<div classname="aie-text" data-inline-style-ranges='${JSON.stringify([
     isr,
-  ])}'><span classname="${styleName}">${text}</span></div>`;
+  ])}'><span classname="${styleName}" style="${cssString}">${text.replace(
+    /[\u200B-\u200F\uFEFF]/g,
+    '',
+  )}</span></div>`;
   // c
 }
