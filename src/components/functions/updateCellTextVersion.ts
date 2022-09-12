638534a3828ab0825f7686fdc885969a0102b909
Paul Thomas <paul@asup.co.uk>
2022-09-12 15:24:25 +0100

    Update preload routines

---
+++ src/components/functions/updateCellTextVersion.ts
@@ -0,0 +1,11 @@
+import { AitCellData } from 'components/ait';
+
+export const UpdateCellTextVersion = (cell: AitCellData): AitCellData => {
+  return {
+    ...cell,
+    text:
+      !cell.text.startsWith('<div') && cell.text !== ''
+        ? `<div classname="aie-text"><span>${cell.text}</span></div>`
+        : cell.text,
+  };
+};
